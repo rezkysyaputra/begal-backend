a25404199d0e4e7a96bf69fe47685d7443f9a66b
rezkysyaputra <muhrezkysyaputra100@gmail.com>
2024-11-10 09:16:56 +0700

    add: NODE_ENV

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,4 +1,3 @@
-// src/utils/logger.ts
 import { createLogger, format, transports } from 'winston';
 import DailyRotateFile from 'winston-daily-rotate-file';
 
@@ -11,21 +10,30 @@ const logFormat = printf(({ level, message, timestamp }) => {
 // Konfigurasi Winston logger
 const logger = createLogger({
   format: combine(
-    colorize(), // Menambahkan warna pada log di konsol
+    colorize(),
     timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     logFormat
   ),
   transports: [
+    // Tambahkan Console logging untuk semua environment
     new transports.Console(),
+  ],
+});
+
+// Hanya tambahkan DailyRotateFile dan File logging di pengembangan lokal
+if (process.env.NODE_ENV === 'development') {
+  logger.add(
     new DailyRotateFile({
       filename: 'logs/application-%DATE%.log',
       datePattern: 'YYYY-MM-DD',
       maxFiles: '14d', // Menyimpan log selama 14 hari
-    }),
-  ],
-});
+    })
+  );
 
-// Log pesan error ke file terpisah
-logger.add(new transports.File({ filename: 'logs/error.log', level: 'error' }));
+  // Log pesan error ke file terpisah hanya di pengembangan
+  logger.add(
+    new transports.File({ filename: 'logs/error.log', level: 'error' })
+  );
+}
 
 export default logger;
