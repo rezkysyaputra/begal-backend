b4d474c3139611810da2a7a4e93f41b4fac8b478
rezkysyaputra <muhrezkysyaputra100@gmail.com>
2024-11-16 04:19:33 +0700

    add: swagger docs for order

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -2,7 +2,52 @@ import { Request, Response, NextFunction } from 'express';
 import { OrderService } from '../services/orderService';
 import { Order } from '../types/orderType';
 
+/**
+ * @swagger
+ * tags:
+ *   name: Order
+ *   description: Order management
+ */
 export class OrderController {
+  /**
+   * @swagger
+   * /api/orders:
+   *   post:
+   *     tags:
+   *       - Order
+   *     summary: Create a new order
+   *     security:
+   *       - bearerAuth: []
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             required:
+   *               - products
+   *               - payment_method
+   *               - seller_id
+   *             properties:
+   *               seller_id:
+   *                 type: string
+   *               payment_method:
+   *                 type: string
+   *               products:
+   *                 type: array
+   *                 items:
+   *                   type: object
+   *                   properties:
+   *                     product_id:
+   *                       type: string
+   *                     quantity:
+   *                       type: integer
+   *     responses:
+   *       201:
+   *         description: Order created successfully
+   *       400:
+   *         description: Bad request
+   */
   static async createOrder(req: Request, res: Response, next: NextFunction) {
     try {
       const user = (req as any).user;
@@ -19,6 +64,20 @@ export class OrderController {
     }
   }
 
+  /**
+   * @swagger
+   * /api/orders:
+   *   get:
+   *     tags: [Order]
+   *     summary: Get all orders
+   *     security:
+   *       - bearerAuth: []
+   *     responses:
+   *       200:
+   *         description: Orders retrieved successfully
+   *       400:
+   *         description: Bad request
+   */
   static async list(req: Request, res: Response, next: NextFunction) {
     try {
       const user = (req as any).user;
@@ -33,6 +92,26 @@ export class OrderController {
     }
   }
 
+  /**
+   * @swagger
+   * /api/orders/{orderId}:
+   *   get:
+   *     tags: [Order]
+   *     summary: Get an order by ID
+   *     security:
+   *       - bearerAuth: []
+   *     parameters:
+   *       - in: path
+   *         name: orderId
+   *         required: true
+   *         schema:
+   *           type: string
+   *     responses:
+   *       200:
+   *         description: Order retrieved successfully
+   *       400:
+   *         description: Bad request
+   */
   static async get(req: Request, res: Response, next: NextFunction) {
     try {
       const user = (req as any).user;
@@ -48,6 +127,36 @@ export class OrderController {
     }
   }
 
+  /**
+   * @swagger
+   * /api/orders/{orderId}:
+   *   patch:
+   *     tags: [Order]
+   *     summary: Update status an order by ID
+   *     security:
+   *       - bearerAuth: []
+   *     parameters:
+   *       - in: path
+   *         name: orderId
+   *         required: true
+   *         schema:
+   *           type: string
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               status:
+   *                 type: string
+   *                 enum: ['pending', 'processing', 'shipped', 'delivered', 'canceled']
+   *     responses:
+   *       200:
+   *         description: Order status updated successfully
+   *       400:
+   *         description: Bad request
+   */
   static async update(req: Request, res: Response, next: NextFunction) {
     try {
       const user = (req as any).user;
