148327ba0373c1258b446a3885214773b0ffe8a3
rezkysyaputra <muhrezkysyaputra100@gmail.com>
2024-11-17 02:25:55 +0700

    fix docs

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,6 +8,7 @@ import { errorLogger, requestLogger } from '../middlewares/loggerMiddleware';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJsDoc from 'swagger-jsdoc';
 import swaggerOptions from '../config/swagger';
+import path from 'path';
 
 const app: express.Application = express();
 
@@ -20,7 +21,49 @@ connectDB();
 
 // Swagger docs
 const swaggerSpec = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Pastikan file static Swagger UI dapat diakses
+app.use(
+  '/api-docs/swagger-ui.css',
+  express.static(
+    path.join(__dirname, 'node_modules/swagger-ui-dist/swagger-ui.css')
+  )
+);
+
+app.use(
+  '/api-docs/swagger-ui-bundle.js',
+  express.static(
+    path.join(__dirname, 'node_modules/swagger-ui-dist/swagger-ui-bundle.js')
+  )
+);
+
+app.use(
+  '/api-docs/swagger-ui-standalone-preset.js',
+  express.static(
+    path.join(
+      __dirname,
+      'node_modules/swagger-ui-dist/swagger-ui-standalone-preset.js'
+    )
+  )
+);
+
+// Setup Swagger UI
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec, {
+    explorer: true,
+    customCssUrl: '/api-docs/swagger-ui.css',
+    customJs: '/api-docs/swagger-ui-bundle.js',
+  })
+);
+
+// Middleware untuk security headers
+app.use((req, res, next) => {
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  next();
+});
 
 // Routes
 app.use('/api', publicRoute);
